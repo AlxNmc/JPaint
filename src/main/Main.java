6e1c8ade0b5ddaf0beafa79e2ea1fcb35b4af341
Alex Niemiec <alxniem@gmail.com>
2019-01-28 11:16:41 -0600

    added MouseHandler class

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,32 +20,33 @@ public class Main {
         ApplicationState appState = new ApplicationState(uiModule);
         IJPaintController controller = new JPaintController(uiModule, appState);
         controller.setup();
+        MouseHandler mouseHandler = new MouseHandler(paintCanvas);
 
         // For example purposes only; remove all lines below from your final project.
-
+        paintCanvas.addMouseListener(mouseHandler);
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Filled in rectangle
-        Graphics2D graphics2d = paintCanvas.getGraphics2D();
-        graphics2d.setColor(Color.GREEN);
-        graphics2d.fillRect(12, 13, 200, 400);
-
-        // Outlined rectangle
-        graphics2d.setStroke(new BasicStroke(5));
-        graphics2d.setColor(Color.BLUE);
-        graphics2d.drawRect(12, 13, 200, 400);
-
-        // Selected Shape
-        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
-        graphics2d.setStroke(stroke);
-        graphics2d.setColor(Color.BLACK);
-        graphics2d.drawRect(7, 8, 210, 410);
-
-        // Clears the Canvas
-        //paintCanvas.repaint();
+//        // Filled in rectangle
+//        Graphics2D graphics2d = paintCanvas.getGraphics2D();
+//        graphics2d.setColor(Color.GREEN);
+//        graphics2d.fillRect(12, 13, 200, 400);
+//
+//        // Outlined rectangle
+//        graphics2d.setStroke(new BasicStroke(5));
+//        graphics2d.setColor(Color.BLUE);
+//        graphics2d.drawRect(12, 13, 200, 400);
+//
+//        // Selected Shape
+//        Stroke stroke = new BasicStroke(3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, 1, new float[]{9}, 0);
+//        graphics2d.setStroke(stroke);
+//        graphics2d.setColor(Color.BLACK);
+//        graphics2d.drawRect(7, 8, 210, 410);
+//
+//        // Clears the Canvas
+//        //paintCanvas.repaint();
     }
 }
